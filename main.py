6c136a0936cc5eb2c39b7158eee2bc7a9a96b82f
Faiz Momin <faizmomin113@yahoo.ca>
2020-08-31 13:56:11 -0400

    added main.py and test image

---
+++ main.py
@@ -0,0 +1,19 @@
+from PIL import Image
+
+ 
+
+# Create an Image object from an Image
+
+imageObject  = Image.open("./whale.jpg")
+
+ 
+
+# Crop the iceberg portion
+
+cropped = imageObject.crop((100,30,400,300))
+
+ 
+
+# Display the cropped portion
+
+imageObject.show()
